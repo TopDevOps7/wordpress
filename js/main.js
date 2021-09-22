58b83fea1ff84c53e8d522fb9b104c2a63be0600
Yelizaveta <79942709+YelizavetaR@users.noreply.github.com>
2021-09-22 19:20:59 +0900

    complete(7/21)

---
+++ js/main.js
@@ -0,0 +1,57 @@
+//active navbar
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function () {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("scroll-on");
+    }else{
+        nav.classList.remove("scroll-on")
+    }
+}
+
+
+//nav hide
+let navBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show")
+    })
+})
+
+//counter design
+document.addEventListener("DOMContentLoaded", () => {
+    function skills(id, start, end, duration){
+        let obj = document.getElementById(id),
+        current = start,
+        range = end - start,
+        increment = end > start ? 1 : -1,
+        step = Math.abs(Math.floor(duration / range)),
+        timer = setInterval(() => {
+            current += increment;
+            obj.textContent = current;
+            if(current == end){
+                clearInterval(timer);
+            }
+        }, step);
+    }
+    skills("counter1", 0, 76, 3000);
+    skills("counter2", 0, 86, 6500);
+    skills("counter3", 0, 93, 5400);
+    skills("counter4", 0, 81, 6600);
+});
+
+
+// Tab content
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
